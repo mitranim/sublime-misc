138ece8ac2973d3c8d9325eba2631133cc4db0d4
Nelo Mitranim <me@mitranim.com>
2022-09-26 13:54:33 +0200

    avoid exception when ST loads test file

--- sublime_misc_test.py
+++ sublime_misc_test.py
@@ -1,36 +1,41 @@
-import unittest as ut
-import sublime_misc_util as u
+def main():
+    import unittest as ut
+    import sublime_misc_util as u
 
-class test_main(ut.TestCase):
-    def test_unquote(t):
-        t.assertEqual(u.unquote(''), '')
-        t.assertEqual(u.unquote('str'), 'str')
-        t.assertEqual(u.unquote('\"str\"'), 'str')
-        t.assertEqual(u.unquote('\"\"str\"\"'), '\"str\"')
-        t.assertEqual(u.unquote('\"\'str\'\"'), '\'str\'')
-        t.assertEqual(u.unquote('\'str\''), 'str')
-        t.assertEqual(u.unquote('\'\'str\'\''), '\'str\'')
-        t.assertEqual(u.unquote('`str`'), 'str')
-        t.assertEqual(u.unquote('``str``'), '`str`')
-        t.assertEqual(u.unquote('`\'str\'`'), '\'str\'')
-        t.assertEqual(u.unquote('`\"str\"`'), '\"str\"')
-        t.assertEqual(u.unquote('\"`str`\"'), '`str`')
-        t.assertEqual(u.unquote('\'`str`\''), '`str`')
+    class test_main(ut.TestCase):
+        def test_unquote(t):
+            t.assertEqual(u.unquote(''), '')
+            t.assertEqual(u.unquote('str'), 'str')
+            t.assertEqual(u.unquote('\"str\"'), 'str')
+            t.assertEqual(u.unquote('\"\"str\"\"'), '\"str\"')
+            t.assertEqual(u.unquote('\"\'str\'\"'), '\'str\'')
+            t.assertEqual(u.unquote('\'str\''), 'str')
+            t.assertEqual(u.unquote('\'\'str\'\''), '\'str\'')
+            t.assertEqual(u.unquote('`str`'), 'str')
+            t.assertEqual(u.unquote('``str``'), '`str`')
+            t.assertEqual(u.unquote('`\'str\'`'), '\'str\'')
+            t.assertEqual(u.unquote('`\"str\"`'), '\"str\"')
+            t.assertEqual(u.unquote('\"`str`\"'), '`str`')
+            t.assertEqual(u.unquote('\'`str`\''), '`str`')
 
-    def test_cycle_quote(t):
-        t.assertEqual(u.cycle_quote(''), '``')
-        t.assertEqual(u.cycle_quote('str'), '`str`')
-        t.assertEqual(u.cycle_quote('\"str\"'), '`str`')
-        t.assertEqual(u.cycle_quote('\"\"str\"\"'), '`\"str\"`')
-        t.assertEqual(u.cycle_quote('\"\'str\'\"'), '`\'str\'`')
-        t.assertEqual(u.cycle_quote('\'str\''), '"str"')
-        t.assertEqual(u.cycle_quote('\'\'str\'\''), '"\'str\'"')
-        t.assertEqual(u.cycle_quote('`str`'), '\'str\'')
-        t.assertEqual(u.cycle_quote('``str``'), '\'`str`\'')
-        t.assertEqual(u.cycle_quote('`\'str\'`'), '\'\'str\'\'')
-        t.assertEqual(u.cycle_quote('`\"str\"`'), '\'\"str\"\'')
-        t.assertEqual(u.cycle_quote('\"`str`\"'), '``str``')
-        t.assertEqual(u.cycle_quote('\'`str`\''), '"`str`"')
+        def test_cycle_quote(t):
+            t.assertEqual(u.cycle_quote(''), '``')
+            t.assertEqual(u.cycle_quote('str'), '`str`')
+            t.assertEqual(u.cycle_quote('\"str\"'), '`str`')
+            t.assertEqual(u.cycle_quote('\"\"str\"\"'), '`\"str\"`')
+            t.assertEqual(u.cycle_quote('\"\'str\'\"'), '`\'str\'`')
+            t.assertEqual(u.cycle_quote('\'str\''), '"str"')
+            t.assertEqual(u.cycle_quote('\'\'str\'\''), '"\'str\'"')
+            t.assertEqual(u.cycle_quote('`str`'), '\'str\'')
+            t.assertEqual(u.cycle_quote('``str``'), '\'`str`\'')
+            t.assertEqual(u.cycle_quote('`\'str\'`'), '\'\'str\'\'')
+            t.assertEqual(u.cycle_quote('`\"str\"`'), '\'\"str\"\'')
+            t.assertEqual(u.cycle_quote('\"`str`\"'), '``str``')
+            t.assertEqual(u.cycle_quote('\'`str`\''), '"`str`"')
+
+    suite = ut.TestSuite()
+    suite.addTests(ut.defaultTestLoader.loadTestsFromTestCase(test_main))
+    ut.TextTestRunner().run(suite)
 
 if __name__ == '__main__':
-    ut.main()
+    main()
